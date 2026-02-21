14083e4d488d6bd565bcf7c8c6576e262e1977c0
pallavid18 <pallavidhope9@gmail.com>
2026-02-21 20:37:33 +0530

    forntend

--- DemoServletProgram/src/main/java/day1/FirstServletTest.java
+++ DemoServletProgram/src/main/java/day1/FirstServletTest.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * Servlet implementation class FirstServletTest
@@ -27,7 +28,11 @@ public class FirstServletTest extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		PrintWriter pw = response.getWriter();
+		pw.println("<html> <body>");
+		pw.println("Welcome to servlet program ");
+		pw.println("</body> </html>");
+		pw.close();
 	}
 
 	/**
---
+++ DemoServletProgram/src/main/java/day1/LifeCycleDemo.java
@@ -0,0 +1,49 @@
+package day1;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/* Servlet implementation class LifeCycleDemo */
+
+@WebServlet("/LifeCycleDemo")
+public class LifeCycleDemo extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	String msg;
+	
+    public LifeCycleDemo() {
+        super();
+        
+    }
+
+	/* @see Servlet#init(ServletConfig) */
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("Init method executed");
+		msg ="Welcome... ";
+	}
+
+	/* @see Servlet#destroy()*/
+	
+	public void destroy() {
+		System.out.println("Destory method Executed");
+	}
+
+	/* @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)*/
+	
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		response.setContentType("text/html");
+		PrintWriter pw = response.getWriter();
+		pw.println("<html><body>");
+		pw.println(msg +"Service method executed");
+		pw.println("</body></html>");
+		pw.close();
+	}
+
+}
